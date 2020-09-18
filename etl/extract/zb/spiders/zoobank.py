6b7dc1d18357351d8eefc14c01b4f2037a1f9193
mguidoti <marcus.guidoti@gmail.com>
2020-09-18 18:55:44 -0300

    feat: Update etl.extract.zb.spiders.zoobank.py
    Start urls, one new field, a logic to define which <ol> element to grab
    per url/year and a logging message were added.

--- etl/extract/zb/spiders/zoobank.py
+++ etl/extract/zb/spiders/zoobank.py
@@ -3,22 +3,42 @@ import scrapy
 from scrapy.loader import ItemLoader
 from zb.items import Doc, ZbItemLoader
 
+import logging
+
 
 class ZoobankSpider(scrapy.Spider):
 
     name = 'zoobank'
     allowed_domains = ['zoobank.org']
 
-    #start_urls = ['http://test.zoobank.org/Search?search_term=2018#publications']
-    start_urls = ['file:///C:/Users/poa/Desktop/ZooBank.org.html']
+    start_urls = [
+       'http://test.zoobank.org/Search?search_term=2010',
+        'http://test.zoobank.org/Search?search_term=2011',
+        'http://test.zoobank.org/Search?search_term=2012',
+        'http://test.zoobank.org/Search?search_term=2013',
+        'http://test.zoobank.org/Search?search_term=2014',
+        'http://test.zoobank.org/Search?search_term=2015',
+        'http://test.zoobank.org/Search?search_term=2016',
+        'http://test.zoobank.org/Search?search_term=2017',
+        'http://test.zoobank.org/Search?search_term=2018',
+        'http://test.zoobank.org/Search?search_term=2019',
+        'http://test.zoobank.org/Search?search_term=2020'
+    ]
+
+    yrs_list = ['2014', '2015', '2016', '2019', '2020']
 
     def parse(self, response):
 
-        # TO DO: 2019 e 2020 needs to start at [2]
-        r = response.xpath('//ol[@class="searchResults"][1]//li')
-        #r = response.xpath('//ol[@class="searchResults"][1]//li/descendant-or-self::*/text()')
+        ol_element = 1
 
-        count = 0
+        if any(year in response.url for year in self.yrs_list):
+            ol_element = 2
+        elif '2018' in response.url:
+            ol_element = 3
+
+        r = response.xpath('//ol[@class="searchResults"][{}]//li'.format(ol_element))
+
+        count_records = 0
 
         for sel in r:
             loader = ZbItemLoader(Doc(), selector=sel)
@@ -31,10 +51,16 @@ class ZoobankSpider(scrapy.Spider):
             loader.add_xpath('issue', 'descendant-or-self::*/text()')
             loader.add_xpath('start_page', 'descendant-or-self::*/text()')
             loader.add_xpath('end_page', 'descendant-or-self::*/text()')
+            loader.add_value('from_year', response.url[-4:])
 
-            loader.load_item()
+            yield loader.load_item()
+            #print(loader.load_item())      # Commented out for now
 
-            count += 1
-            print(loader.item)
+            count_records += 1
 
-        print("\nTotal of... %s records." % count)
+        log_message = "Total of {} records for year {}.".format(count_records,
+            response.url[-4:])
+
+        print(log_message)
+
+        logging.warning(log_message)
