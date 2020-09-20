ebe0c17181febebb693f85132ad1abe45419fd77
mguidoti <marcus.guidoti@gmail.com>
2020-09-20 01:53:50 -0300

    feat: Updates etl.transform.sherpa_romeo
    Added a safety lock to avoid crashing with bad requests. Also added one
    additional for loop, because I realized that I could have more data
    being missed on the previous logic (and it proved to be the case). And
    I created a function to selectivelly title-case the journals's names, to
     be applied before calling SHERPA/RoMEo, because I noticed their API is
    also case sensitive.

--- etl/transform/sherpa_romeo.py
+++ etl/transform/sherpa_romeo.py
@@ -1,6 +1,6 @@
 import requests
 
-from config import SHERPA_KEY
+from etl.transform.config import SHERPA_KEY
 
 # List of journals used to test the function
 '''
@@ -25,27 +25,51 @@ def check_oa(jrnls_list):
 
         url = ('https://v2.sherpa.ac.uk/cgi/retrieve?item-type=publication&format=Json&'
         'limit=10&filter=%5B%5B%22title%22%2C%22equals%22%2C%20%22{journal}%22%5D%'
-        '5D&api-key={key}'.format(key=SHERPA_KEY, journal=jrnl))
+        '5D&api-key={key}'.format(key=SHERPA_KEY, journal=titlecase(jrnl)))
 
-        r = requests.get(url)
-        data = r.json()
+        # Cold bloaded safety lock here. If requests fails, then, no value for
+        # that specific journal
+        try:
+            r = requests.get(url)
+            data = r.json()
+        except:
+            results[jrnl] = None
 
         try:
-            info = data['items'][0]['publisher_policy'][0]['permitted_oa']
+            info = data['items'][0]['publisher_policy']
 
             for each in info:
 
-                if 'published' in each['article_version']:
+                oa = each['permitted_oa']
 
-                    if each['additional_oa_fee'] == 'yes':
-                        results[jrnl] = 'Closed-access'
+                for elem in oa:
+
+                    if 'published' in elem['article_version']:
+
+                        if elem['additional_oa_fee'] == 'yes':
+                            results[jrnl] = 'Closed-access'
+                        else:
+                            results[jrnl] = 'GOLD|DIAMOND'
                     else:
-                        results[jrnl] = 'GOLD|DIAMOND'
-                else:
-                    pass
+                        pass
         except:
             results[jrnl] = None
 
     return results
 
-# print(check_oa(journals))         # Testing print
+def titlecase(journal):
+
+    words = journal.split(' ')       # re.split behaves as expected
+    final = [words[0].capitalize()]
+
+    for word in words[1:]:
+
+        final.append(word if word in
+            ['of', 'the', 'to',
+            'de', 'la', 'del', 'di',
+            'for', 'and', 'in'] else word.capitalize())
+
+    return " ".join(final)
+
+#print(check_oa(['Zoological Journal of the Linnean Society']))         # Testing print
+
